3b56c618763aafb18c5400607a7595c1237ae67b
Mamadou Bhoye Bah <bhoyemad11@gmail.com>
2025-03-21 18:46:21 +0000

    header nav

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,5 +3,12 @@ const menuToggle = document.getElementById('menu-toggle');
 const navbar = document.querySelector('.navbar');
 menuToggle.addEventListener('click', () => {
     navbar.classList.toggle('show');
-    navbar.classList.toggle('active');
+      // Changer l'icône entre "fa-bars" et "fa-times"
+      if (navbar.classList.contains('show')) {
+        menuToggle.classList.remove('fa-bars');
+        menuToggle.classList.add('fa-times');
+    } else {
+        menuToggle.classList.remove('fa-times');
+        menuToggle.classList.add('fa-bars');
+    }
 });
